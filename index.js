7589e045947c2a8d20e1e0a323c946cd394c36b2
Govind0926V <sharmagovind8487@gmail.com>
2025-04-04 20:44:54 +0530

    login page added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const { DiffieHellmanGroup } = require("crypto");
+// filepath: d:\Hackbyte3\Hackbyte3\index.js
+const express = require('express');
+const path = require('path');
 
-DiffieHellmanGroup
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve login.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
